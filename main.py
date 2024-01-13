8bd0742a86305249043ee8e38aa146368138c393
Troy Dutton <45671229+Bushvacka@users.noreply.github.com>
2024-01-12 20:52:07 -0600

    Refactored imports

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import logging
 
-from mcts import MCTS
 from model import ResNet
-from train import ACTION_SIZE, NUM_PLANES, ChessDataset
+from train import ChessDataset
 
 logging.basicConfig(
     format="%(asctime)s.%(msecs)03d %(message)s", level=logging.INFO, datefmt="%H:%M:%S"
@@ -11,14 +10,12 @@ logging.basicConfig(
 
 def main():
     dataset = ChessDataset()
-    # dataset.load_pgn_directory("resources/examples")
-    # dataset.save("resources/examples/dataset.h5")
-    dataset.load("resources/examples/dataset.h5")
+    dataset.load_pgn_directory("resources/examples")
+    dataset.save("resources/examples/dataset.h5")
+    # dataset.load("resources/examples/dataset.h5")
 
-    model = ResNet(
-        in_channels=NUM_PLANES, num_actions=ACTION_SIZE, depth=12, device="cpu"
-    )
-    model.fit(dataset, epochs=4, batch_size=800)
+    model = ResNet()
+    model.fit(dataset, epochs=10, batch_size=1024)
     model.save("resources/models/model.pth")
 
 
